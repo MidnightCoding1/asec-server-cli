226b51acf7cd9fd620164def619d8adf139455a1
MidnightCoding <Calimali2006@gmail.com>
2026-02-17 20:38:46 +0100

    Correct syntax and enhance command handling in asec.sh
    Fix syntax errors and add missing commands in asec.sh

--- asec.sh
+++ asec.sh
@@ -2,8 +2,10 @@
 
 ASEC_ROOT="$(cd "$(dirname "$0")" && pwd)"
 
+# load modules
 . "$ASEC_ROOT/core/version.sh"
 . "$ASEC_ROOT/core/help.sh"
+. "$ASEC_ROOT/core/system.sh"
 
 CMD="$1"
 
@@ -13,6 +15,9 @@ case "$CMD" in
     ;;
   "version")
     asec_version
+    ;;
+  "system")
+    asec_system_status
     ;;
   *)
     echo "[asec] unknown command: $CMD"
