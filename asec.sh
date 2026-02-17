55d7255e09b3c6e2c02b34cd51874969b853b593
MidnightCoding <Calimali2006@gmail.com>
2026-02-17 04:27:15 +0100

    Add basic command handling to asec.sh

--- asec.sh
+++ asec.sh
@@ -1,1 +1,21 @@
+#!/bin/sh
 
+ASEC_ROOT="$(cd "$(dirname "$0")" && pwd)"
+
+. "$ASEC_ROOT/core/version.sh"
+. "$ASEC_ROOT/core/help.sh"
+
+CMD="$1"
+
+case "$CMD" in
+  "" | "help")
+    asec_help
+    ;;
+  "version")
+    asec_version
+    ;;
+  *)
+    echo "[asec] unknown command: $CMD"
+    echo "use: asec help"
+    ;;
+esac
