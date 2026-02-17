cb9eb1d87db25024498c56357494dc4954c8c1ed
MidnightCoding <Calimali2006@gmail.com>
2026-02-17 04:28:10 +0100

    Implement help function for asec-server-cli
    Add help function to display usage information

--- core/help.sh
+++ core/help.sh
@@ -1,1 +1,16 @@
+asec_help() {
+  cat <<'EOF'
+asec-server-cli
 
+USAGE:
+  asec <command>
+
+COMMANDS:
+  help        show this help menu
+  version     show version info
+
+INFO:
+  terminal-based server & security toolkit
+  linux & termux compatible
+EOF
+}
